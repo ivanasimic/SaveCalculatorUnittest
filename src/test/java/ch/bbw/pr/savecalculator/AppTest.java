bee8c7ee858256bd78ab58519d2c0112c931a1ed
Simic Ivana, GHR-OSA-NEX-24 <Ivana.Simic@swisscom.com>
2022-01-22 12:44:36 +0100

    erste Probetests gemacht

--- src/test/java/ch/bbw/pr/savecalculator/AppTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/AppTest.java
@@ -1,8 +1,10 @@
 package ch.bbw.pr.savecalculator;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.Before;
 
 /**
  * Unit test for simple App.
@@ -12,9 +14,34 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
+    private SaveCalculator test;
+
+    @Before
+    public void createSavCalculatorTeste() {
+        test = new SaveCalculator();
+    }
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+
+
+    //erste Probe Tests
+    @Test
+    public void testSummeZweiPositiveIsOk() {
+        int summand1 = 25;
+        int summand2 = 10;
+        assertEquals(35, test.summe(summand1, summand2));
+    }
+
+    @Test
+    public void testSubtraktionZweiPositiveIsOk() {
+        int minuend1 = 25;
+        int minuend2 = 10;
+        assertEquals(15, test.subtraktion(minuend1, minuend2));
+    }
 }
+
