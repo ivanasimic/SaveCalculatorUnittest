bcfcf476f7af799f2e247dc0e18a646054e3ecff
Simic Ivana, GHR-OSA-NEX-24 <Ivana.Simic@swisscom.com>
2022-01-24 19:30:34 +0100

    JAR aufrufen

--- src/main/java/ch/bbw/pr/savecalculator/App.java
+++ src/main/java/ch/bbw/pr/savecalculator/App.java
@@ -9,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "The Save Calculation!" );
         System.out.println("Integer.MAX_VALUE: " + Integer.MAX_VALUE);
         System.out.println("MAX_VALUE + MAX_VALUE: " + (Integer.MAX_VALUE + Integer.MAX_VALUE));
